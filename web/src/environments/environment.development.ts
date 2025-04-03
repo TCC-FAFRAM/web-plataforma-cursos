089a6ea052b4bb2195e84447c94f31064278542e
Mateus Borges <51982343+devmateusborges@users.noreply.github.com>
2025-04-02 21:47:04 -0300

    biold

--- web/src/environments/environment.development.ts
+++ web/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   versao: '1.0.0',
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://100.80.191.98:3000/api',
   appKey: '',
   appName: 'Plataforma Cursos Online',
 };
